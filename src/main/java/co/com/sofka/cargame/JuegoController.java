04f7af411c1e73047cd7fa8383908085d8847501
JoseCruzAvila <jose.cruz@sofka.com.co>
2022-07-02 15:30:10 -0500

    Add cross origin for requests

--- src/main/java/co/com/sofka/cargame/JuegoController.java
+++ src/main/java/co/com/sofka/cargame/JuegoController.java
@@ -13,14 +13,12 @@ import co.com.sofka.domain.generic.DomainEvent;
 import co.com.sofka.infraestructure.asyn.SubscriberEvent;
 import co.com.sofka.infraestructure.repository.EventStoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins="*")
 public class JuegoController {
     @Autowired
     private SubscriberEvent subscriberEvent;
